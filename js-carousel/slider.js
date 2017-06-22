d85b094f570219f08f4b1e0256958700062d3fd5
DanCouper <danielcouper81@googlemail.com>
2017-06-22 12:22:05 +0100

    Cleaned up slider JS
    - Changed sanity checks for elements.
    - Inlined default options in class.
    - Added transition opts configurable from data attributes.
    - Lazy loaded all but first image.

--- js-carousel/slider.js
+++ js-carousel/slider.js
@@ -1,111 +1,103 @@
-const sliderDefaults = {
-  intervalTime: 4000
-};
-
 /**
  * Creates and initialises a slider. If the slider is not
- * fully initialised, the page will just render a single slide,
+ * fully initialised, the page shouold just render a single slide,
  * with no controls.
- *
- * Boot happens in a series of steps:
- *
- *   - The slider script is attached to an element.
- *   - Options are read from the data attributes, falling back to defaults.
- *   - TODO The templates containing the remaining slides are activated
- *     in sequence: this means the images contained can be lazily
- *     loaded well after initial page render.
- *   - If the prev/next controls are to be used, the template containing
- *     them is activated.
- *   - If autoplay is to be used, the template containing the
- *     autoplay controls (play/pause) is activated and the
- *     interval initialised.
- * @type {[type]}
  */
 
 class Slider {
   constructor(element) {
+    const sliderDefaults = {
+      intervalTime: 4000,
+      transitionDelay: '0s',
+      transitionDuration: '.75s',
+      transitionTimingFunction: 'cubic-bezier(0.550, 0.055, 0.675, 0.190)'
+    };
+
     // DOM selection:
     this.slider = document.querySelector(element);
     this.slidesContainer = this.slider.querySelector('.slides');
     this.slides = this.slider.getElementsByClassName('slide');
-    // Template selection (these may not exist):
-    this.sliderControlsPrevTemplate = this.slider.querySelector('.slider-controls-prev-template');
-    this.sliderControlsNextTemplate = this.slider.querySelector('.slider-controls-next-template');
-    this.sliderAutoplayControlsPlayTemplate = this.slider.querySelector('.slider-autoplay-controls-play-template');
-    this.sliderAutoplayControlsPauseTemplate = this.slider.querySelector('.slider-autoplay-controls-pause-template');
+
     // DOM controls, undefined until controls are loaded (these may not exist):
-    this.sliderControlPrev;
-    this.sliderControlNext;
-    this.sliderControlPlay;
-    this.sliderControlPause;
+    this.sliderControlPrev = this.slider.querySelector('.slider-control-prev');
+    this.sliderControlNext = this.slider.querySelector('.slider-control-next');
+    this.sliderControlPlay = this.slider.querySelector('.slider-control-play');
+    this.sliderControlPause = this.slider.querySelector('.slider-control-pause');
 
-    const opts = Object.assign(this.slider.dataset, sliderDefaults);
+    // For the controls to work, both prev and next _must_ be present.
+    // for the autoplay to work, pause and play _must_ be present.
+    this.hasControls = !!this.sliderControlPrev && !!this.sliderControlNext;
+    this.canAutoplay = !!this.sliderControlPlay && !!this.sliderControlPause;
+    // Immediately blow up if there are no controls at all:
+    if (!this.hasControls && !this.canAutoplay) throw new Error('Slider controls cannot be located in the DOM');
 
-    // Slider controls state:
-    this.hasControls = Boolean(this.sliderControlsPrevTemplate) && Boolean(this.sliderControlsNextTemplate); // cast object to bool to check for existance
-    this.canAutoplay = Boolean(this.sliderAutoplayControlsPlayTemplate) && Boolean(this.sliderAutoplayControlsPauseTemplate); // cast object to bool to check for existance
+    this.opts = Object.assign({}, this.slider.dataset, sliderDefaults);
 
-    if (!this.hasControls && !this.canAutoplay) throw new Error('Slider has neither controls nor autoplay functionality so cannot act as a slider.');
 
-    this.intervalTime = opts.intervalTime; // Amount of time in ms each slide should be visible
+    this.intervalTime = this.opts.intervalTime; // Amount of time in ms each slide should be visible
     this.currentSlide = 1; // The slides are 1-indexed, and current slide always starts at 1
-    this.numSlides = 1; // The number of slides inits at 1, and is corrected when remaining slides are loaded
+    this.numSlides = this.slides.length; // NOTE only initial slide image should load until document fully ready
     this.isPlaying = false; // use to check if autoplay timer should be reset when next/prev slide is clicked
     this.interval; // initially undefined, reference used to the autoplay interval
   }
 
-  loadSlides() {
-    // TODO stub function, this method should lazy load the remaining slides
-    this.numSlides = this.slides.length;
+  loadRemainingSlides() {
+    this.slidesContainer.querySelectorAll('img[data-src]').forEach(img => {
+      img.setAttribute('src', img.dataset.src);
+      img.onload = () => img.removeAttribute('data-src');
+    });
   }
 
+  loadControls() {
+    // If there is only one slide (or none), no controls need be rendered:
+    if (this.numSlides < 2) {
+      if (this.hasControls) {
+        [this.sliderControlPrev, this.sliderControlNext].forEach(ctrl => ctrl.setAttribute('aria-hidden', 'true'));
+      }
+      if (this.canAutoplay) {
+        [this.sliderControlPlay, this.sliderControlPause].forEach(ctrl => ctrl.setAttribute('aria-hidden', 'true'));
+      }
+    }
+  }
 
   /**
    * Transitions are inherently tied to the slider UI, and depend upon
    * the number of slides to accurately calculate the % change in position.
    * The setup function applies the correct % width for the overall
-   * constainer, and the apply function is ran each time a next/prev
-   * handler is triggered to apply the corect trnslation.
+   * constainer, then immedaitely applies a translateX value to
+   * position the slides.
    */
   setupTransitions() {
-    this.slidesContainer.style.width = `${this.numSlides * 100}%`;
-    this.applyTransitions();
+    Object.assign(this.slidesContainer.style, {
+      width: `${this.numSlides * 100}%`,
+      transitionDelay: this.opts.transitionDelay,
+      transitionDuration: this.opts.transitionDuration,
+      transitionProperty: 'transform',
+      transitionTimingFunction: this.opts.transitionTimingFunction,
+    });
+
+    this.applyTranslation();
   }
 
-  applyTransitions() {
+  applyTranslation() {
     const translation = `translateX(-${(this.currentSlide - 1) * (100 / this.numSlides)}%)`;
     this.slidesContainer.style.transform = translation;
   }
 
-  loadControls() {
-    if (this.hasControls && this.numSlides > 1) {
-      this.slider.appendChild(this.sliderControlsPrevTemplate.content.cloneNode(true));
-      this.slider.appendChild(this.sliderControlsNextTemplate.content.cloneNode(true));
-      // Add the control references to the state:
-      this.sliderControlPrev = this.slider.getElementsByClassName('slider-control-prev')[0];
-      this.sliderControlNext = this.slider.getElementsByClassName('slider-control-next')[0];
-    }
-  }
-
-  loadAutoplayControls() {
-    if (this.canAutoplay && this.numSlides > 1) {
-      this.slider.appendChild(this.sliderAutoplayControlsPlayTemplate.content.cloneNode(true));
-      this.slider.appendChild(this.sliderAutoplayControlsPauseTemplate.content.cloneNode(true));
-      // Add the autoplay control references to the state:
-      this.sliderControlPlay = this.slider.getElementsByClassName('slider-control-play')[0];
-      this.sliderControlPause = this.slider.getElementsByClassName('slider-control-pause')[0];
-    }
-  }
-
+  /**
+   * [showCurrent description]
+   * @return {[type]} [description]
+   */
   showCurrent() {
+    // Switch aria-hidden on and off for accessibility reasons - the
+    // non-visible slides should be hidden from accessibility tools.
     [...this.slides].forEach((slide, i) => {
-      this.applyTransitions();
-      if (i + 1 === this.currentSlide) {
-        slide.setAttribute('aria-hidden','false');
-      } else {
-        slide.setAttribute('aria-hidden','true');
-      }
+      // NOTE Slides are 1-indexed, so when looping through the
+      // slides, need to compare against i + 1.
+      slide.setAttribute('aria-hidden', (i + 1 === this.currentSlide) ? 'false' : 'true');
     });
+
+    this.applyTranslation();
   }
 
   handleNextSlide(e) {
@@ -148,15 +140,14 @@ class Slider {
 
     // Set up the autoplay after everything, including images, has loaded:
     window.addEventListener('load', () => {
+      this.loadRemainingSlides();
       if (this.canAutoplay) this.handlePlaySlides();
     });
   }
 
   init() {
-    this.loadSlides();
-    this.setupTransitions();
     this.loadControls();
-    this.loadAutoplayControls();
+    this.setupTransitions();
     this.bindEvents();
   }
 }
